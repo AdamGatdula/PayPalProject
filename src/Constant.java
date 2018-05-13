1243a435cb887045152a2fa887febc816c15496c
AdamGatdula <adam.gatdula1@gmail.com>
2018-05-13 19:35:14 +0300

    test try

--- src/Constant.java
+++ src/Constant.java
@@ -2,5 +2,6 @@ public class Constant {
     public static final String EMAIL = "email";
     public static final String PASSWORD = "password";
     public static final String REPASSWORD = "confirmPassword";
+	//jhyfdhf
 
 }
